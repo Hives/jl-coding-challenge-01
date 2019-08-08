da9e4aa2e94d0fb56d8b50ba96af1a99cf265b52
Paul Martin <communty.hivemind@gmail.com>
2019-08-08 22:03:13 +0100

    Refactor
    Replaces `formatMinutes()` and `formatSeconds()` with a more general
    method, `extractUnit()`.
    
    `extractUnit()` seems not that good though... it reduces `seconds` each
    time which is maybe not that clear when you look at it

--- src/main/kotlin/time formatter.kt
+++ src/main/kotlin/time formatter.kt
@@ -1,31 +1,29 @@
+private data class Unit(val name: String, val numberOfSeconds: Int)
+private data class UnitQuantity(val quantity: Int, val description: String)
+private var seconds = 0
+
 fun formatTime(input: Int): String {
     if (input == 0) return "none"
 
-    val units:List<UnitQuantity> = listOf(
-        formatMinutes(input),
-        formatSeconds(input)
-    )
+    seconds = input
 
-    return joinUnits(units)
+    val unitQuantityDescriptions = units.map { extractUnit(it) }
+        .filter { it.quantity > 0 }
+        .map { it.description }
+
+    return unitQuantityDescriptions.joinToString(" and ")
 }
 
-private data class UnitQuantity(val quantity: Int, val description: String)
+private val units = listOf(
+    Unit("minute", 60),
+    Unit("second", 1)
+)
 
-private fun formatSeconds(input: Int): UnitQuantity {
-    val name = "second"
-    val quantity = input % 60
-    val description = pluralise("$quantity $name", quantity)
+private fun extractUnit(unit: Unit): UnitQuantity {
+    val quantity = seconds / unit.numberOfSeconds
+    val description = "$quantity " + pluralise(unit.name, quantity)
+    seconds -= quantity * unit.numberOfSeconds
     return UnitQuantity(quantity, description)
 }
-
-private fun formatMinutes(input: Int): UnitQuantity {
-    val name = "minute"
-    val quantity = input / 60
-    val description = pluralise("$quantity $name", quantity)
-    return UnitQuantity(quantity, description)
-}
-
-private fun joinUnits(units: List<UnitQuantity>): String =
-    units.filter { it.quantity > 0 }.joinToString(" and ") { it.description }
 
 private fun pluralise(string: String, number: Int) = if (number == 1) string else string + "s"
