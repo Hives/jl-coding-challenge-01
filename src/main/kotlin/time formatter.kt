b7b785b5380477a9b329ed18ce8518a475cab1e0
Paul Martin <communty.hivemind@gmail.com>
2019-08-08 17:49:32 +0100

    Refactor
    Extract method to pluralise a string as necessary

--- src/main/kotlin/time formatter.kt
+++ src/main/kotlin/time formatter.kt
@@ -14,16 +14,18 @@ private data class UnitQuantity(val quantity: Int, val description: String)
 private fun formatSeconds(input: Int): UnitQuantity {
     val name = "second"
     val quantity = input % 60
-    val description = if (quantity == 1) "$quantity $name" else "$quantity ${name}s"
+    val description = pluralise("$quantity $name", quantity)
     return UnitQuantity(quantity, description)
 }
 
 private fun formatMinutes(input: Int): UnitQuantity {
     val name = "minute"
     val quantity = input / 60
-    val description = if (quantity == 1) "$quantity $name" else "$quantity ${name}s"
+    val description = pluralise("$quantity $name", quantity)
     return UnitQuantity(quantity, description)
 }
 
 private fun joinUnits(units: List<UnitQuantity>): String =
     units.filter { it.quantity > 0 }.joinToString(" and ") { it.description }
+
+private fun pluralise(string: String, number: Int) = if (number == 1) string else string + "s"
