2728a23ffb09bcf146451dff18dedc5c6cd1d541
Paul Martin <communty.hivemind@gmail.com>
2019-08-08 23:12:40 +0100

    Tweaks

--- src/main/kotlin/time formatter.kt
+++ src/main/kotlin/time formatter.kt
@@ -1,11 +1,11 @@
 private data class Unit(val name: String, val numberOfSeconds: Int)
 private data class UnitQuantity(val quantity: Int, val description: String)
-private var seconds = 0
+private var remainder = 0
 
 fun formatTime(input: Int): String {
     if (input == 0) return "none"
 
-    seconds = input
+    remainder = input
 
     val quantityDescriptions = units.map { extractUnit(it) }
         .filter { it.quantity > 0 }
@@ -15,27 +15,27 @@ fun formatTime(input: Int): String {
 }
 
 private val units = listOf(
-    Unit("year", 60 * 60 * 24 * 365),
-    Unit("day", 60 * 60 * 24),
-    Unit("hour", 60 * 60),
+    Unit("year",   60 * 60 * 24 * 365),
+    Unit("day",    60 * 60 * 24),
+    Unit("hour",   60 * 60),
     Unit("minute", 60),
     Unit("second", 1)
 )
 
 private fun extractUnit(unit: Unit): UnitQuantity {
-    val quantity = seconds / unit.numberOfSeconds
+    val quantity = remainder / unit.numberOfSeconds
     val description = pluralise("$quantity ${unit.name}", quantity)
-    seconds -= quantity * unit.numberOfSeconds
+    remainder -= quantity * unit.numberOfSeconds
     return UnitQuantity(quantity, description)
 }
 
 private fun pluralise(string: String, number: Int) = if (number == 1) string else string + "s"
 
 private fun joinDescriptions(descriptions: List<String>): String =
-    if (descriptions.size > 2) {
-        val firstDescriptions = descriptions.subList(0, descriptions.size - 1).joinToString()
-        val lastDescription = descriptions.last()
-        listOf(firstDescriptions, lastDescription).joinToString(" and ")
+    if (descriptions.size >= 3) {
+        val firstOnes = descriptions.subList(0, descriptions.size - 1).joinToString(", ")
+        val lastOne = descriptions.last()
+        listOf(firstOnes, lastOne)
     } else {
-        descriptions.joinToString(" and ")
-    }
+        descriptions
+    }.joinToString(" and ")
