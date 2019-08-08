8a23c632b9568acd1a2e8132d78e53b55ebebeaf
Paul Martin <communty.hivemind@gmail.com>
2019-08-08 22:46:05 +0100

    Refactor - extract `joinDescriptions()` method

--- src/main/kotlin/time formatter.kt
+++ src/main/kotlin/time formatter.kt
@@ -11,14 +11,7 @@ fun formatTime(input: Int): String {
         .filter { it.quantity > 0 }
         .map { it.description }
 
-    if (quantityDescriptions.size > 2) {
-        return listOf(
-            quantityDescriptions.subList(0, quantityDescriptions.size - 1).joinToString(),
-            quantityDescriptions.last()
-        ).joinToString(" and ")
-    } else {
-       return quantityDescriptions.joinToString(" and ")
-    }
+    return joinDescriptions(quantityDescriptions)
 }
 
 private val units = listOf(
@@ -29,9 +22,18 @@ private val units = listOf(
 
 private fun extractUnit(unit: Unit): UnitQuantity {
     val quantity = seconds / unit.numberOfSeconds
-    val description = "$quantity " + pluralise(unit.name, quantity)
+    val description = pluralise("$quantity ${unit.name}", quantity)
     seconds -= quantity * unit.numberOfSeconds
     return UnitQuantity(quantity, description)
 }
 
 private fun pluralise(string: String, number: Int) = if (number == 1) string else string + "s"
+
+private fun joinDescriptions(descriptions: List<String>): String =
+    if (descriptions.size > 2) {
+        val firstDescriptions = descriptions.subList(0, descriptions.size - 1).joinToString()
+        val lastDescription = descriptions.last()
+        listOf(firstDescriptions, lastDescription).joinToString(" and ")
+    } else {
+        descriptions.joinToString(" and ")
+    }
