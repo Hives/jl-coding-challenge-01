32a8288c618fdb480ad569cefa65c132c90ef0ef
Paul Martin <communty.hivemind@gmail.com>
2019-08-09 08:20:08 +0100

    Get rid of `Name` data class
    Just put the info about a unit's name straight into the `Unit` data
    class

--- src/main/kotlin/time formatter.kt
+++ src/main/kotlin/time formatter.kt
@@ -1,3 +1,5 @@
+private var remainingSeconds = 0
+
 fun formatTime(input: Int): String {
     if (input == 0) return "none"
 
@@ -11,16 +13,14 @@ fun formatTime(input: Int): String {
     return joinDescriptions(quantityDescriptions)
 }
 
-private data class Name(val singular: String, val plural: String)
-private data class Unit(val name: Name, val numberOfSeconds: Int)
-private var remainingSeconds = 0
+private data class Unit(val singularName: String, val pluralName: String, val numberOfSeconds: Int)
 
 private val units = listOf(
-    Unit(Name("year",   "years"),   60 * 60 * 24 * 365),
-    Unit(Name("day",    "days"),    60 * 60 * 24),
-    Unit(Name("hour",   "hours"),   60 * 60),
-    Unit(Name("minute", "minutes"), 60),
-    Unit(Name("second", "seconds"), 1)
+    Unit("year",   "years",   60 * 60 * 24 * 365),
+    Unit("day",    "days",    60 * 60 * 24),
+    Unit("hour",   "hours",   60 * 60),
+    Unit("minute", "minutes", 60),
+    Unit("second", "seconds", 1)
 )
 
 private fun getQuantityOfUnit(unit: Unit): Int {
@@ -33,7 +33,7 @@ private fun makeHumanReadable(quantity: Int, unit: Unit): String? =
     if (quantity == 0) {
         null
     } else {
-        "$quantity " + if (quantity == 1) unit.name.singular else unit.name.plural
+        "$quantity " + if (quantity == 1) unit.singularName else unit.pluralName
     }
 
 private fun joinDescriptions(descriptions: List<String>): String =
